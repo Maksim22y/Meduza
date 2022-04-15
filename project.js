ccf518eba4ebe95255bbedb6d648b459a0deb580
Maksim22y <irasnat2013@gmail.com.com>
2022-04-15 18:01:31 +0300

    let поки що рандом букви

--- project.js
+++ project.js
@@ -3,9 +3,9 @@ let screen
 let a=[]//перше число 
 let sig=''//знак 
 let b=''//друге
-let siggn=['=','+','-','x','/','.']
+let siggn=['+','-','x','/','.'] 
 let btn
-let buttons=['0','1','2','3','4','5','6','7','8','9','<-','+','=']
+let buttons=['0','1','2','3','4','5','6','7','8','9']
 
 
 
@@ -13,7 +13,9 @@ let buttons=['0','1','2','3','4','5','6','7','8','9','<-','+','=']
 //  function yyy(){
 //      document.querySelector("body > div.calc > div.screen > p").append(div.btn.nine)
 //      }
+let u=''
 let result=''
+let i=''
 function vvod(){
 btn = document.querySelectorAll('.butons div');
 screen = document.querySelector('body > div.calc > div.screen > p');
@@ -23,17 +25,35 @@ screen = document.querySelector('body > div.calc > div.screen > p');
   function get(btn){
   	btn.addEventListener('click', function(){
     screen.innerHTML =btn.innerHTML;
-     
     })
-  
   }
-  if(buttons.includes(screen.innerHTML)){ result+=screen.innerHTML
-  
+  if(buttons.includes(screen.innerHTML) && u=='' ){
+  result+=screen.innerHTML}
+  else if (siggn.includes(screen.innerHTML) ){
+    u=screen.innerHTML
   }
-if(screen.innerHTML=='='){screen.innerHTML=result}
+  else if (buttons.includes(screen.innerHTML) && u!==''){
+i+=screen.innerHTML
+  }
+// if (screen.innerHTML=='+')
+if(screen.innerHTML=='='){alert (result),alert(u),alert(i)}
+if
 }
-// if (siggn.includes(e.innerHTML))alert ('tru')
+
+//    if(buttons.includes(screen.innerHTML) ) { result+=screnerHTML en.in
+//     } 
+//     if( screen.innerHTML=='+') {b+=screen.innerHTML}
+// alert(b)
+// // function iii(){
+// //    b+=screen.innerHTML
+// //   }//alert(b)
+//  if(screen.innerHTML=='='){screen.innerHTML=(result)}
+
+
+  
  
+// if (siggn.includes(e.innerHTML))alert ('tru')
+
 
 
 // document.addEventListener("click", function(e) {
