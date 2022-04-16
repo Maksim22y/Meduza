1bdfe0ced1c5af6472539d8703d83c7bd10d63ca
Maksim22y <irasnat2013@gmail.com.com>
2022-04-16 18:10:51 +0300

    a

--- project.js
+++ project.js
@@ -1,11 +1,10 @@
 
 let screen 
-let a=[]//перше число 
+//перше число 
 let sig=''//знак 
-let b=''//друге
-let siggn=['+','-','x','/','.'] 
+let siggn=['+','-','x','/'] 
 let btn
-let buttons=['0','1','2','3','4','5','6','7','8','9']
+let buttons=['0','1','2','3','4','5','6','7','8','9','.']
 
 
 
@@ -13,9 +12,11 @@ let buttons=['0','1','2','3','4','5','6','7','8','9']
 //  function yyy(){
 //      document.querySelector("body > div.calc > div.screen > p").append(div.btn.nine)
 //      }
-let u=''
+let result1=''
 let result=''
-let i=''
+let sign=''
+let a=''
+let b=''
 function vvod(){
 btn = document.querySelectorAll('.butons div');
 screen = document.querySelector('body > div.calc > div.screen > p');
@@ -24,19 +25,56 @@ screen = document.querySelector('body > div.calc > div.screen > p');
    
   function get(btn){
   	btn.addEventListener('click', function(){
-    screen.innerHTML =btn.innerHTML;
+
+  screen.innerHTML=btn.innerHTML
+      // result+=btn.innerHTML
+      // function unic(arr){
+      //   let iii=[]
+      // for(let str of arr){
+      //   if(!iii.includes(str)){
+      //     iii.push(str)
+      //   }return iii}
+        
+      // }
+      // screen.innerHTML=unic.result
+
+      
+     
     })
+    
   }
-  if(buttons.includes(screen.innerHTML) && u=='' ){
-  result+=screen.innerHTML}
+  if(buttons.includes(screen.innerHTML) && sign=='' ){
+  a+=screen.innerHTML}
   else if (siggn.includes(screen.innerHTML) ){
-    u=screen.innerHTML
+    sign=screen.innerHTML
   }
-  else if (buttons.includes(screen.innerHTML) && u!==''){
-i+=screen.innerHTML
+  else if (buttons.includes(screen.innerHTML) && sign!==''){
+b+=screen.innerHTML
   }
-// if (screen.innerHTML=='+')
-if(screen.innerHTML=='='){alert (result),alert(u),alert(i)}
+  result+=screen.innerHTML
+ 
+  // if (screen.innerHTML=='+')
+if(screen.innerHTML=='='){
+switch(sign){
+  case '-':
+  screen.innerHTML=result+(+a-(+b))
+  break 
+  case '+':
+    screen.innerHTML=result+(+a+(+b))
+    break
+    case '/':
+      screen.innerHTML=result+(a/b).toFixed(1)
+      break 
+      case 'x':
+        screen.innerHTML=result+(a*b).toFixed(1)
+        break
+
+}
+result=''
+a='',
+b='',
+sign=''
+}
 }
 
 //    if(buttons.includes(screen.innerHTML) ) { result+=screnerHTML en.in
