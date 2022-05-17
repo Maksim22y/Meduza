c77f49ac33bd2c5d3a8219fa78f3561e3bfd02ec
Maksim22y <irasnat2013@gmail.com.com>
2022-05-17 21:43:21 +0300

    1

--- project.js
+++ project.js
@@ -1,9 +1,11 @@
+heightBtn = document.querySelector('.calc')
+heightScreen = document.querySelector('.screen')
+let btn =document.querySelectorAll("body > div > div.butons > div.btn")
+let screen = document.querySelector('body > div.calc > div.screen > p');
 
-let screen 
 //перше число 
 let sig=''//знак 
 let siggn=['+','-','x','/'] 
-let btn
 let buttons=['0','1','2','3','4','5','6','7','8','9','.']
 
 
@@ -12,23 +14,16 @@ let buttons=['0','1','2','3','4','5','6','7','8','9','.']
 //  function yyy(){
 //      document.querySelector("body > div.calc > div.screen > p").append(div.btn.nine)
 //      }
-let result1=''
-let result=''
-let sign=''
-let a=''
-let b=''
-function vvod(){
-  heightBtn=document.querySelector('.calc')
-  heightScreen=document.querySelector('.screen')
-btn = document.querySelectorAll('.butons div');
-screen = document.querySelector('body > div.calc > div.screen > p');
-  
-  btn.forEach(get);
-   
-  function get(btn){
-  	btn.addEventListener('click', function(){
+let result1 = ''
+let result = ''
+let sign = ''
+let a = ''
+let b = ''
 
-  screen.innerHTML=btn.innerHTML
+btn.forEach(bbb=>{
+  bbb.addEventListener('click',function(){
+    screen.innerHTML=bbb.innerHTML
+    
       // result+=btn.innerHTML
       // function unic(arr){
       //   let iii=[]
@@ -42,27 +37,26 @@ screen = document.querySelector('body > div.calc > div.screen > p');
 
       
      
-    })
     
-  }
   // document.addEventListener("click", function(e) {
   //   if (e.target.className=="butons") {
-      
-  //    //ваши действия
+  //     alert() 
+  //     return
+     
   //   }
-  // }); //якщо попадаєш на кнопки     
- 
-  if(buttons.includes(screen.innerHTML) && sign==''){
-  a+=screen.innerHTML }
-  
-  else if (siggn.includes(screen.innerHTML)&& a!==''){
-    sign+=screen.innerHTML
+  // }); //якщо не попадаєш на кнопки     
+  if (buttons.includes(screen.innerHTML) && sign == '') {
+    a += screen.innerHTML
+  }
+
+  else if (siggn.includes(screen.innerHTML) && a !== '') {
+    sign += screen.innerHTML
   }
   // else if (sign[0].includes('-')){
   //   // sign=sign[1]
   // alert (sign[1])}
-  else if (buttons.includes(screen.innerHTML) && sign!==''){
-b+=screen.innerHTML
+  else if (buttons.includes(screen.innerHTML) && sign !== '') {
+    b += screen.innerHTML
   }
 
 
@@ -73,24 +67,26 @@ b+=screen.innerHTML
   // else if (sign.length>1){a=result
   // }
 
-  result+=screen.innerHTML 
-  if (result.includes('C')){result=''
-  a='',
-  b='',
-  sign=''}
+  result += screen.innerHTML
+  if (result.includes('C')) {
+    result = ''
+    a = '',
+      b = '',
+      sign = ''
+  }
 
-  if(result.includes('&lt')){
-     result=result.slice(0,-5)
-  if(b!==''){
-b=b.slice(0,-1)
-  }
-  else if (b=='' && sign!==''){
-    sign=sign.slice(0,-1)
-  }
-  else if (b==''&& sign=='') {
-    a=a.slice(0,-1)
-  }
-   } 
+  if (result.includes('&lt')) {
+    result = result.slice(0, -5)
+    if (b !== '') {
+      b = b.slice(0, -1)
+    }
+    else if (b == '' && sign !== '') {
+      sign = sign.slice(0, -1)
+    }
+    else if (b == '' && sign == '') {
+      a = a.slice(0, -1)
+    }
+  } 
 //    alert (a)
 //    alert(b)
 // alert(sign)
@@ -215,7 +211,7 @@ if (screen.innerHTML.length>20){heightScreen.style.height='250px'
 // } 
 // alert(a,sign,b)
 
-}
+
 //    if(buttons.includes(screen.innerHTML) ) { result+=screnerHTML en.in
 //     } 
 //     if( screen.innerHTML=='+') {b+=screen.innerHTML}
@@ -229,5 +225,14 @@ if (screen.innerHTML.length>20){heightScreen.style.height='250px'
   
  
 // if (siggn.includes(e.innerHTML))alert ('tru')
+
+
+
+  }
+  
+  ) 
+})
+
+
 
 
